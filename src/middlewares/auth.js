f2b12981b3b543c190267ca24082fb051ec4618c
Ponyo <fathur.abdanalfi11@gmail.com>
2020-10-14 13:46:57 +0700

    update

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,7 @@ const helpers = require('../helpers/helpers')
 module.exports = {
     verifyAccess: (req, res, next) => {
         let token = req.headers.authorization
-        if(!token) return helpers.response(res, {message: 'Token is needed'})
+        if(!token) return helpers.response(res, {message: 'Token is needed'}, 403)
         token = token.split(" ")[1]
         jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
             const message = {message: 'Token is invalid'}
